74bf633d18b97509e09d6aca3289a3c52c836170
Cristian David Home <chome@codebranch.co>
2025-07-11 15:24:28 -0500

    Refactor: Update sidebar item paths to use constants for better maintainability

--- src/lib/constants/UI/SideBarItems.ts
+++ src/lib/constants/UI/SideBarItems.ts
@@ -1,24 +1,32 @@
 import { NavBarItem } from "../../types/NavBarItems";
+import {
+  HOME,
+  ABOUT,
+  MORE_SUB_ITEM_1,
+  MORE_SUB_ITEM_2,
+  PROFILE,
+  SETTINGS,
+} from "@/lib/constants/routes/Dashboard";
 
 export const sideBarItems: NavBarItem[] = [
   {
     text: "Home",
-    path: "/",
+    path: HOME,
     icon: "home",
   },
   {
     text: "About",
-    path: "/about",
+    path: ABOUT,
     icon: "information",
   },
   {
     text: "Profile",
-    path: "/profile",
+    path: PROFILE,
     icon: "user-settings",
   },
   {
     text: "Settings",
-    path: "/settings",
+    path: SETTINGS,
     icon: "settings",
   },
   {
@@ -27,11 +35,11 @@ export const sideBarItems: NavBarItem[] = [
     subItems: [
       {
         text: "Sub Item 1",
-        path: "/more/sub-item-1",
+        path: MORE_SUB_ITEM_1,
       },
       {
         text: "Sub Item 2",
-        path: "/more/sub-item-2",
+        path: MORE_SUB_ITEM_2,
       },
     ],
   },
