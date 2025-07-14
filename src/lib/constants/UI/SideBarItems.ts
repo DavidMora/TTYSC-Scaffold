a6da442229c537a1de4ce422388cb7d77d0aad9b
Cristian David Home <chome@codebranch.co>
2025-07-14 16:37:32 -0500

    Refactor: Remove commented-out sidebar item definitions for cleaner code

--- src/lib/constants/UI/SideBarItems.ts
+++ src/lib/constants/UI/SideBarItems.ts
@@ -1,69 +1,9 @@
 import { NavBarItem } from "../../types/NavBarItems";
-import {
-  MORE_SUB_ITEM_1,
-  MORE_SUB_ITEM_2,
-} from "@/lib/constants/routes/Dashboard";
 
 export const sideBarItems: NavBarItem[] = [
   // {
   //   text: "Settings",
   //   icon: "action-settings",
-  //   subItems: [
-  //     {
-  //       text: "Sub Item 1",
-  //       path: MORE_SUB_ITEM_1,
-  //     },
-  //     {
-  //       text: "Sub Item 2",
-  //       path: MORE_SUB_ITEM_2,
-  //     },
-  //   ],
-  // },
-  // {
-  //   text: "Definitions",
-  //   icon: "attachment-text-file",
-  //   subItems: [
-  //     {
-  //       text: "Sub Item 1",
-  //       path: MORE_SUB_ITEM_1,
-  //     },
-  //     {
-  //       text: "Sub Item 2",
-  //       path: MORE_SUB_ITEM_2,
-  //     },
-  //   ],
-  // },
-  // {
-  //   text: "Feedback",
-  //   icon: "notification-2",
-  //   subItems: [
-  //     {
-  //       text: "Sub Item 1",
-  //       path: MORE_SUB_ITEM_1,
-  //     },
-  //     {
-  //       text: "Sub Item 2",
-  //       path: MORE_SUB_ITEM_2,
-  //     },
-  //   ],
-  // },
-  // {
-  //   text: "Raw Data",
-  //   icon: "it-host",
-  //   subItems: [
-  //     {
-  //       text: "Sub Item 1",
-  //       path: MORE_SUB_ITEM_1,
-  //     },
-  //     {
-  //       text: "Sub Item 2",
-  //       path: MORE_SUB_ITEM_2,
-  //     },
-  //   ],
-  // },
-  // {
-  //   text: "Chat History",
-  //   icon: "timesheet",
   //   subItems: [
   //     {
   //       text: "Sub Item 1",
